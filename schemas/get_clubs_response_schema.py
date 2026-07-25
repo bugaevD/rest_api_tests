927f11939dde75b57f4aea85d54e19c91c9d0a39
bugaev <bugaev1404g@mail.ru>
2026-07-25 15:49:34 +0300

    Add few simple tests

---
+++ schemas/get_clubs_response_schema.py
@@ -0,0 +1,76 @@
+status_response = {
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "title": "Generated schema for Root",
+  "type": "object",
+  "properties": {
+    "count": {
+      "type": "number"
+    },
+    "next": {
+      "type": "string"
+    },
+    "previous": {},
+    "results": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "id": {
+            "type": "number"
+          },
+          "bookTitle": {
+            "type": "string"
+          },
+          "bookAuthors": {
+            "type": "string"
+          },
+          "publicationYear": {
+            "type": "number"
+          },
+          "description": {
+            "type": "string"
+          },
+          "telegramChatLink": {
+            "type": "string"
+          },
+          "owner": {
+            "type": "number"
+          },
+          "members": {
+            "type": "array",
+            "items": {
+              "type": "number"
+            }
+          },
+          "reviews": {
+            "type": "array",
+            "items": {}
+          },
+          "created": {
+            "type": "string"
+          },
+          "modified": {}
+        },
+        "required": [
+          "id",
+          "bookTitle",
+          "bookAuthors",
+          "publicationYear",
+          "description",
+          "telegramChatLink",
+          "owner",
+          "members",
+          "reviews",
+          "created",
+          "modified"
+        ]
+      }
+    }
+  },
+  "required": [
+    "count",
+    "next",
+    "previous",
+    "results"
+  ]
+}
---
+++ test_book_club_status.py
@@ -0,0 +1,80 @@
+import requests
+from jsonschema import validate
+
+from schemas.club_schema import club_response
+from schemas.get_clubs_response_schema import status_response
+from schemas.search_schema import search_title_schema
+
+
+def test_count_of_clubs_with_schema():
+    response = requests.get("https://book-club.qa.guru/api/v1/clubs/")
+
+    print("\nStatus code: ", response.status_code)
+    print("Body: ", response.text)
+
+    assert response.status_code == 200
+
+    body = response.json()
+    validate(body, schema=status_response)
+
+    assert body["count"] == 92
+
+
+def test_club_1_book_title():
+    response = requests.get("https://book-club.qa.guru/api/v1/clubs/1")
+    print("\nStatus code: ", response.status_code)
+    print("Body: ", response.text)
+
+    assert response.status_code == 200
+
+    body = response.json()
+    validate(body, schema=club_response)
+
+    assert body["bookTitle"] == "Of Mice and Men"
+
+
+def test_number_of_clubs_per_page_with_page_size_10():
+    response = requests.get("https://book-club.qa.guru/api/v1/clubs/?page=1&page_size=10")
+
+    print("\nStatus code: ", response.status_code)
+    print("Body: ", response.text)
+
+    assert response.status_code == 200
+
+    body = response.json()
+    validate(body, schema=status_response)
+
+    total_of_clubs_per_page = 0
+
+    for club in body["results"]:
+        total_of_clubs_per_page += 1
+    print("Total number of clubs: ", total_of_clubs_per_page)
+
+    assert total_of_clubs_per_page == 10
+
+
+def test_search_by_book_title():
+    response = requests.get("https://book-club.qa.guru/api/v1/clubs/?search=The%20Other%20Side%20of%20Silence")
+
+    print("\nStatus code: ", response.status_code)
+    print("Body: ", response.text)
+
+    assert response.status_code == 200
+
+    body = response.json()
+    validate(body, schema=search_title_schema)
+
+    assert body["results"][0]["bookTitle"] == "The Other Side of Silence"
+
+
+def test_non_existent_book_title():
+    response = requests.get("https://book-club.qa.guru/api/v1/clubs/?search=Non+existent+book")
+
+    print("\nStatus code: ", response.status_code)
+    print("Body: ", response.text)
+
+    assert response.status_code == 200
+
+    body = response.json()
+
+    assert body["count"] == 0
